f6c6c94a7fe8202f2b0cb597e9d22540e393be35
sachira-madhushan <sacheeramadushan455@gmail.com>
2025-03-28 15:39:01 +0530

    Login User

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,28 +4,52 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function register(Request $request){
-        $fields=$request->validate([
-            "name"=>"required|string",
-            "email"=>"required|email|unique:users",
-            "password"=>"confirmed|required"
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            "name" => "required|string",
+            "email" => "required|email|unique:users",
+            "password" => "confirmed|required"
         ]);
 
-        $user=User::create($fields);
+        $user = User::create($fields);
 
-        $token=$user->createToken($user->name);
+        $token = $user->createToken($user->name);
 
         return response()->json([
-            "message"=>"Registration success",
-            "user"=>$user,
-            "token"=>$token->plainTextToken
+            "message" => "Registration success",
+            "user" => $user,
+            "token" => $token->plainTextToken
 
         ]);
-
     }
 
+    public function login(Request $request)
+    {
+        $request->validate([
+            "email" => "required|email",
+            "password" => "required"
+        ]);
 
+        $user = User::where('email', $request->email)->first();
+
+        if ($user || Hash::check($request->password, $user->password)) {
+            $token = $user->createToken($user->name);
+
+            return response()->json([
+                "message" => "Login success",
+                "user" => $user,
+                "token" => $token->plainTextToken
+
+            ]);
+        } else {
+            return response()->json([
+                "message" => "Incorrect email or password"
+            ], 404);
+        }
+    }
 }
